f63d645351f70b0ac27d40cf350c0d9faea752bc
clubd <danenrmar@gmail.com>
2023-12-07 10:10:45 -0500

    wrap div to video for overlay purpose

--- src/pages/InitialPage/InitialPage.js
+++ src/pages/InitialPage/InitialPage.js
@@ -4,10 +4,16 @@ import Intro from "../../assets/videos/intro.mp4";
 
 function InitialPage() {
     return (
-        <section  className='initial'>
-        <video className='initial__video' src={Intro} autoPlay loop muted />
-        <button className='initial__button'>Registrarse</button>
-        <button className='initial__button'>Iniciar Sesion</button>
+        <section className='initial'>
+            <div className='initial__video-container'>
+                <div className='initial__overlay'>
+                    <video className='initial__video' src={Intro} autoPlay loop muted />
+                </div>
+            </div>
+            <div className='initial__content'>
+                <button className='initial__button'>Registrarse</button>
+                <button className='initial__button'>Iniciar Sesion</button>
+            </div>
         </section>
     )
 }
