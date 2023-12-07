db54ffa177a9770d0423d1d13a017810c7b7c2fd
clubd <danenrmar@gmail.com>
2023-12-06 22:22:28 -0500

    add video asset for testing

---
+++ src/pages/InitialPage/InitialPage.js
@@ -0,0 +1,12 @@
+import './InitialPage.scss';
+import Intro from '../../assets/videos/'
+
+function InitialPage() {
+    return (
+        <>
+        <video src='' autoPlay loop muted />
+        </>
+    )
+}
+
+export default InitialPage
