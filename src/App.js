4607fccbc99a88e338ccea0d5ce56b89ee302763
clubd <danenrmar@gmail.com>
2023-12-16 23:15:13 -0500

    added the sigupn ppage to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import InitialPage from './pages/InitialPage/InitialPage';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
 
 function App() {
   return (
+
     <div>
-      <InitialPage />
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<InitialPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
